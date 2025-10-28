a3bc0864b1533c2b5813de1308062b8d4733bba9
DylanSteinecke <58346782+DylanSteinecke@users.noreply.github.com>
2025-10-28 13:09:03 -0700

    Update script to extract EUR siblings from WGS and BGEN

--- subset_plink_files.sh
+++ subset_plink_files.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set +e +u +o pipefail
 
-# RAP to extract siblings
-UKB_BFILE_DIR="/mnt/project/Bulk/DRAGEN WGS/DRAGEN population level WGS variants, PLINK format [500k release]"
+# RAP to extract most EUR siblings from imputed, convert to bfiles
+# "The alleles in the imputation are aligned with REF/ALT, first_allele is the ref allele on the fwd strand."
+# https://biobank.ndph.ox.ac.uk/ukb/label.cgi?id=100319 
+UKB_BGEN_DIR="/mnt/project/Bulk/Imputation/UKB imputation from genotype"
 for CHR in {1..22}; do
-       plink2 --pfile "${UKB_BFILE_DIR}/ukb24308_c${CHR}_b0_v1" \
+       plink2 --bgen "${UKB_BGEN_DIR}/ukb22828_c${CHR}_b0_v3" ref-first \
+              --make-bed \
+              --extract ~/sibreg/sibreg_project/processed/snp_list_of_snps_with_ldscores.snplist \
+              --keep ~/sibreg/sibreg_project/processed/two_eur_sibs_per_fam_sibs_except_five_bad_chr_person_list.txt \
+              --threads 15 \
+              --out ~/eur_sibs_snps_with_ldscores_orig_imputed_chr${CHR}
+done
+
+# RAP to extract most EUR siblings from WGS, convert to bfiles
+UKB_PFILE_DIR="/mnt/project/Bulk/DRAGEN WGS/DRAGEN population level WGS variants, PLINK format [500k release]"
+for CHR in {1..22}; do
+       plink2 --pfile "${UKB_PFILE_DIR}/ukb24308_c${CHR}_b0_v1" \
               --make-bed \
               --extract range ~/sibreg/sibreg_project/processed/snps_with_ldsc_scores_snp_coord_table.tsv \
               --keep ~/sibreg/sibreg_project/processed/two_eur_sibs_per_fam_sibs_except_five_bad_chr_person_list.txt \
               --threads 15 \
               --no-pheno \
-              --out ~/eur_sibs_snps_with_ldscores_chr${CHR}
+              --out ~/eur_sibs_snps_with_ldscores_wgs_chr${CHR}
 done
